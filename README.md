2a36b10c8b5709607a4ca4f1ee9a6574548a6412
Alan Berndt <alan@eatabrick.org>
2013-06-26 12:12:40 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,26 @@
+# NAME
+
+Net::MPD - Blah blah blah
+
+# SYNOPSIS
+
+    use Net::MPD;
+
+# DESCRIPTION
+
+Net::MPD is
+
+# AUTHOR
+
+Alan Berndt <alan@eatabrick.org>
+
+# COPYRIGHT
+
+Copyright 2013- Alan Berndt
+
+# LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# SEE ALSO
---
+++ lib/Net/MPD.pm
@@ -0,0 +1,39 @@
+package Net::MPD;
+
+use strict;
+use 5.008_005;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+Net::MPD - Blah blah blah
+
+=head1 SYNOPSIS
+
+  use Net::MPD;
+
+=head1 DESCRIPTION
+
+Net::MPD is
+
+=head1 AUTHOR
+
+Alan Berndt E<lt>alan@eatabrick.orgE<gt>
+
+=head1 COPYRIGHT
+
+Copyright 2013- Alan Berndt
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 SEE ALSO
+
+=cut
